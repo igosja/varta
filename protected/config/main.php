8444bd2648ebbe768fa1bc8556b5f819babb5aa3
Igor Boyko <igosja@mail.ru>
2017-05-13 08:12:38 +0000

    main.php edited online with Bitbucket

--- protected/config/main.php
+++ protected/config/main.php
@@ -41,10 +41,10 @@ return array(
             ),
         ),
         'db' => array(
-            'connectionString' => 'mysql:host=localhost;dbname=igosja_varta',
+            'connectionString' => 'mysql:host=vartist.mysql.ukraine.com.ua;dbname=vartist_db',
             'emulatePrepare' => true,
-            'username' => 'igosja_varta',
-            'password' => '&WKWp@*{',
+            'username' => 'vartist_db',
+            'password' => 'ZkspCZFZ',
             'charset' => 'utf8',
         ),
         'messages' => array(
